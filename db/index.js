0e99e18b0881f69044e8b19cead7471fc1870623
Admin Server <apostlma@ucsd.edu>
2022-11-30 12:43:07 +0000

    updated db/index.js file

--- db/index.js
+++ db/index.js
@@ -1,11 +1,20 @@
 const Pool = require('pg').Pool
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    port: '5432',
-    password: 'PosgresRocks123!',
-    database: 'stellantis'
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PW,
+    port: process.env.DB_PORT
+    // user: 'postgres',
+    // host: 'localhost',
+    // port: '5432',
+    // password: 'PosgresRocks123!',
+    // database: 'stellantis'
 })
 
 module.exports = pool;
